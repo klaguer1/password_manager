c7891a58ab2f1225483186fca5de6428f72203ba
KevinLaguerre <kevinlaguerre7689@gmail.com>
2021-05-19 22:49:26 -0400

    Updated CLI components and updated git ignore

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import tkinter as tk
 import argparse 
+import utils
 from desktop_app import DesktopApp 
 
 #To DO: Assuming the website exists put in a feature that finds the website even if the .com or .org is missing. 
@@ -11,6 +12,10 @@ from desktop_app import DesktopApp
 def parse_args():
     parser = argparse.ArgumentParser(description='Generate a secure password') 
     subparsers = parser.add_subparsers(help='commands', dest='command') 
+    
+    #Arguments 
+    parser.add_argument("website", help="The website you are creating a password for")
+    parser.add_argument("email", help="The email or username associated with the password")
 
     # COMMANDS
     # headless
@@ -21,9 +26,13 @@ def parse_args():
 args, parser = parse_args()
 
 if args.command == 'headless':
-     print('Running headless...')
+     print('Running headless...') 
+     pwd = utils.generate_passsword(None, True) 
+     """Add in code to check display whether or not password has been generated
+     or if an error has been encountered from command line. Check if save completed""" 
+    #  print("This is your random password {} for this website: {} and email: {}".format(pwd, args.website, args.email))
     #  parser.print_usage()
-     parser.print_help()   
+    #  parser.print_help()   
 # ---------------------------- DESKTOP APP ------------------------------- # 
 else:  
     root = tk.Tk()
--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import json
 import pyperclip 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-def generate_passsword(passwordEntry):
+def generate_passsword(passwordEntry, headless=False):
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 
     'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 
     'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -16,60 +16,110 @@ def generate_passsword(passwordEntry):
     
     shuffle(passwordList)
     completePassword = "".join(passwordList) 
+    
+    if headless==True: 
+        return display_password(completePassword, passwordEntry, True)
 
-    display_password(completePassword, passwordEntry)
+    else:
+        display_password(completePassword, passwordEntry)
     
 # ---------------------------- DISPLAY PASSWORD ------------------------------- # 
-def display_password(password, passwordEntry): 
+def display_password(password, passwordEntry, headless=False): 
+    if headless == True: 
+        return password 
 
-    passwordEntry.delete(0, "end")
-    passwordEntry.insert(0, password)
-    pyperclip.copy(password) 
+    else:   
+        passwordEntry.delete(0, "end")
+        passwordEntry.insert(0, password)
+        pyperclip.copy(password) 
 
 # ---------------------------- GET PASSWORD ------------------------------- # 
-def get_info(siteEntry, messageBox):
-    user_input = siteEntry.get()
-    try: 
-        with open("data.json", "r") as file:
-            passwords = json.load(file)    
-            selected_password = passwords[user_input]
-            messageBox.showinfo(title = "Password", 
-            message = " Email: {} \n Password: {}".format(selected_password["email"], selected_password["password"]))
-    except (FileNotFoundError, KeyError):
-        messageBox.showerror(title= "Error", message= "No Password Found!")
+def get_info(siteEntry, messageBox, headless=False): 
+    if headless==True:  
+        user_input=siteEntry
+        try: 
+            with open("data.json", "r") as file:
+                passwords = json.load(file)    
+                selected_password = passwords[user_input], 
+                message = " Email: {} \n Password: {}".format(selected_password["email"], selected_password["password"]) 
+                return message
+        except (FileNotFoundError, KeyError):
+            message = "No Password Found!"
+            return message 
+
+    else: 
+        user_input = siteEntry.get()
+        try: 
+            with open("data.json", "r") as file:
+                passwords = json.load(file)    
+                selected_password = passwords[user_input]
+                messageBox.showinfo(title = "Password", 
+                message = " Email: {} \n Password: {}".format(selected_password["email"], selected_password["password"]))
+        except (FileNotFoundError, KeyError):
+            messageBox.showerror(title= "Error", message= "No Password Found!")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def save_password(siteEntry, contactEntry, passwordEntry, messageBox):   
-    siteValue = siteEntry.get() 
-    contactValue = contactEntry.get() 
-    passwordValue = passwordEntry.get() 
-    new_data = {
-        siteValue:
-        {   "email": contactValue,
-            "password": passwordValue,
+def save_password(siteEntry, contactEntry, passwordEntry, messageBox, headless=False):    
+    if headless == True: 
+        siteValue = siteEntry
+        contactValue = contactEntry
+        passwordValue = passwordEntry 
+        success = "Data sucessfully saved"
+        new_data = {
+            siteValue:
+            {   "email": contactValue,
+                "password": passwordValue,
 
+            }
         }
-    }
+        try:
+            with open("data.json", "r") as data_file:
+                #Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4) 
+                return success 
+        else:
+            #Updating old data with new data
+            data.update(new_data)
+            with open("data.json", "w") as data_file:
+                #Saving updated data
+                json.dump(data, data_file, indent=4)
+                return success
+                
 
-    if len(siteValue) == 0 or len(passwordValue) == 0: 
-        messageBox.showwarning(title = "Error", message = "Website and Password fields cannot be blank.") 
-    else:
-        is_ok = messageBox.askokcancel(title = siteValue, message = 
-        "These are the details entered: \nWebsite: {} \nEmail: {}, \nPassword: {} \n Is this ok to save?".format(siteValue, contactValue, passwordValue)) 
-        if is_ok:
-            try:
-                with open("data.json", "r") as data_file:
-                    #Reading old data
-                    data = json.load(data_file)
-            except FileNotFoundError:
-                with open("data.json", "w") as data_file:
-                    json.dump(new_data, data_file, indent=4)
-            else:
-                #Updating old data with new data
-                data.update(new_data)
-                with open("data.json", "w") as data_file:
-                    #Saving updated data
-                    json.dump(data, data_file, indent=4)
-            finally:
-                siteEntry.delete(0, 'end')
-                passwordEntry.delete(0, 'end')
+    else: 
+        siteValue = siteEntry.get() 
+        contactValue = contactEntry.get() 
+        passwordValue = passwordEntry.get() 
+        new_data = {
+            siteValue:
+            {   "email": contactValue,
+                "password": passwordValue,
+
+            }
+        }
+
+        if len(siteValue) == 0 or len(passwordValue) == 0: 
+            messageBox.showwarning(title = "Error", message = "Website and Password fields cannot be blank.") 
+        else:
+            is_ok = messageBox.askokcancel(title = siteValue, message = 
+            "These are the details entered: \nWebsite: {} \nEmail: {}, \nPassword: {} \n Is this ok to save?".format(siteValue, contactValue, passwordValue)) 
+            if is_ok:
+                try:
+                    with open("data.json", "r") as data_file:
+                        #Reading old data
+                        data = json.load(data_file)
+                except FileNotFoundError:
+                    with open("data.json", "w") as data_file:
+                        json.dump(new_data, data_file, indent=4)
+                else:
+                    #Updating old data with new data
+                    data.update(new_data)
+                    with open("data.json", "w") as data_file:
+                        #Saving updated data
+                        json.dump(data, data_file, indent=4)
+                finally:
+                    siteEntry.delete(0, 'end')
+                    passwordEntry.delete(0, 'end')
